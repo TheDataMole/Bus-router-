222dc31437a748f6a096e76bdb3b7e1518e45ca3
Emma Ruth Daly <60703785+EmmaD194@users.noreply.github.com>
2021-04-20 15:11:07 +0100

    read File

--- shortestPath.java
+++ shortestPath.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -6,38 +7,69 @@ import java.util.Scanner;
 public class shortestPath {
 	
 	int stop_id;
-	ArrayList<Edge> mapList[];
-	int speeds[] = new int[3];
-	PriorityQueue<Node> queue = new PriorityQueue<Node>();
+	int adjMatrix[][] = new int[3][3];
 	double distance[];
+	String stop_times;
+	String transfers;
+
+	shortestPath(String stop_times, String transfers){
+		try {
+			readFile(stop_times, transfers);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		createMap();
+
+	}
+
+	void createMap(){
+		
+	}
+
 	
-	void readFile(String file) throws Exception {
-		Scanner scanner = new Scanner(new File(file));
+	void readFile(String file, String file1) throws FileNotFoundException  {
+
+		int departureStop;
+		int arrivalStop;
+		int tripId = 0;
+		int previousTripId = 0;
+		int weight = 1;
+		File stopTimes = new File (stop_times);
+		Scanner scanner = new Scanner(stopTimes);
+		scanner.nextLine();
+		String line;
+
+		while(scanner.hasNextLine()){
+			line = scanner.nextLine();
+			Scanner currentLine = new Scanner(line);
+			currentLine.useDelimiter(",");
+
+			tripId = currentLine.nextInt();
+			arrivalStop = currentLine.nextInt();
+
+			if(previousTripId == tripId){
+				adjMatrix[departureStop][arrivalStop] = weight;
+			}
+
+
+
+		}
 		
 		if (!scanner.hasNextInt()) {
 			return;
 		}
 		
-		stop_id = scanner.nextInt();
-		mapList = new ArrayList[stop_id];
-		for (int i = 0; i < stop_id; i++) {
-			mapList[i] = new ArrayList();
-			
-		}
-		distance = new double[stop_id];
 		
-		int times = scanner.nextInt();
-		while (times > 0) {
-			times--;
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			double time_to_stop = a - b;
-			mapList[a].add(new Edge(a, b, time_to_stop));
-		}
+		
+	}
+
+	public static void main(String[] args) {
+		shortestPath map = new shortestPath("stop_times.txt", "transfers.txt");
 	}
 }
-
-class Edge{
+/*
+ class Edge{
 	public int u, v;
     public double length;
     public Edge(int u, int v, double length) {
@@ -47,20 +79,20 @@ class Edge{
     }
 }
     
-class Node implements Comparable<Node>{
+class Stop implements Comparable<Stop>{
     public int u;
     public double time;
-    public Node(int u, double time) {
+    public Stop(int u, double time) {
          this.u = u;
          this.time = time;
     }
-    public int compareTo(Node node) {
-         if (time < node.time) {
+    public int compareTo(Stop stop) {
+         if (time < stop.time) {
               return -1; 
           }
-          if (time > node.time) {
+          if (time > stop.time) {
               return 1; 
           }
           return 0; 
     }
-}
+}*/
